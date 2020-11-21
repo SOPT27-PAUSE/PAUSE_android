0f397dccb5065b991f0deec214c29251568bae5f
hyooosong <gythd1998@gmail.com>
2020-11-22 04:59:45 +0900

    Create Record Layout

--- app/src/main/java/com/example/pause_android/RecordActivity.kt
+++ app/src/main/java/com/example/pause_android/RecordActivity.kt
@@ -2,10 +2,34 @@ package com.example.pause_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import kotlinx.android.synthetic.main.activity_record.*
 
 class RecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
+
+//        val NoOfEmp = ArrayList<Int>()
+//
+//        NoOfEmp.run {
+//            add(0,10)
+//            add(1,13)
+//            add(2,20)
+//        }
+//
+//        val year = ArrayList<Int>()
+//
+//        year.run {
+//            add(16)
+//            add(17)
+//            add(18)
+//        }
+//        val barDataSet = BarDataSet(NoOfEmp,"bo")
+//        graph_time.animateY(180)
+//        val data = BarData(year, barDataSet)
+//        barDataSet.colors(#80EBE4)
+//        graph_time.data(data)
     }
 }
