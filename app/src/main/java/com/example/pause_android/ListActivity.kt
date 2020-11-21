f21486e4ac09ebba34a2324422b80fcfd5e154af
jinyand <jiiin97@naver.com>
2020-11-22 07:13:40 +0900

    [현진] 플레이리스트 통신 완료

--- app/src/main/java/com/example/pause_android/ListActivity.kt
+++ app/src/main/java/com/example/pause_android/ListActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_list.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,9 +25,100 @@ class ListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
+        requestList("ASMR")
+
+        btn_go_record.setOnClickListener {
+            val intent = Intent(this, RecordActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        activity_list_rv_video.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            if(scrollY > oldScrollY) {
+                val animation = TranslateAnimation(0F, 0F, 0F, 500F)
+                animation.duration = 1000
+
+                animation.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {
+                        btn_stop.visibility = GONE
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        btn_stop.visibility = GONE
+                    }
+                    override fun onAnimationRepeat(animation: Animation?) { }
+                })
+                btn_stop.startAnimation(animation)
+            }
+
+            if(scrollY <oldScrollY) {
+                val topani = TranslateAnimation(0F, 0F, 500F, 0F)
+                topani.duration=1000
+
+                topani.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {
+                        btn_stop.visibility = VISIBLE
+                    }
+                    override fun onAnimationEnd(animation: Animation?) {
+
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) { }
+                })
+                btn_stop.startAnimation(topani)
+            }
+        }
+
+
+
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(spinner.getItemAtPosition(position)) {
+                    "먹방" -> {
+                        activity_list_tv_category.text = "먹방"
+                    }
+                    "예능" -> {
+                        activity_list_tv_category.text = "예능"
+                    }
+                    "음악" -> {
+                        activity_list_tv_category.text = "음악"
+                        requestList("음악")
+                    }
+                    "드라마" -> {
+                        activity_list_tv_category.text = "드라마"
+                    }
+                    "ASMR" -> {
+                        activity_list_tv_category.text = "ASMR"
+                        requestList("ASMR")
+                    }
+                    "패션" -> {
+                        activity_list_tv_category.text = "패션"
+                    }
+                    "게임" -> {
+                        activity_list_tv_category.text = "게임"
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
+    }
+
+    private fun requestList(category : String) {
         requestToServer.service.requestPlaylist(
             playtime = 30,
-            category = "예능"
+            category = category
         ).enqueue(
             object : Callback<ResponseListData> {
                 override fun onResponse(
--- app/src/main/java/com/example/pause_android/ListViewHolder.kt
+++ app/src/main/java/com/example/pause_android/ListViewHolder.kt
@@ -11,11 +11,13 @@ class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
     val image = itemView.findViewById<ImageView>(R.id.img_thumbnail)
     val title = itemView.findViewById<TextView>(R.id.tv_video_title)
     val count = itemView.findViewById<TextView>(R.id.tv_video_count)
+    val playtime = itemView.findViewById<TextView>(R.id.tv_video_time)
 
     @SuppressLint("SetTextI18n")
     fun bind(videoData: VideoData) {
         Glide.with(itemView).load(videoData.image).into(image)
         title.text = videoData.title
         count.text = videoData.count.toString() + "개"
+        playtime.text = videoData.playtime.toString() + ":00"
     }
 }
